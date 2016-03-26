6b08b70a051ca489c270028276d89bf2cf2d0ced
A2ZH <thejianmail@gmail.com>
2016-03-26 12:59:22 +0800

    highlight search

--- 25/javascript.js
+++ 25/javascript.js
@@ -1,16 +1,23 @@
 (function () {
   "use script";
   
+  var tree = document.getElementById('tree-panel');
   var panelHead = document.getElementsByClassName('tree-panel_head')[0];
   panelHead.addEventListener('click', (e) => {
     e.stopPropagation();
+    
     if(e.target.className === 'search-button') {
-      console.log('search');
+      let input = document.getElementsByClassName('search-text')[0];
+      let searchString = input.value;
+      if(searchString) {
+        input.value = "";
+        DFS(tree, searchString);
+      }
     } else if (e.target.className === 'add-button') {
       let nodeName = prompt("New Node Name");
       if(nodeName) {
         let node = createNode(nodeName);
-        document.getElementById('tree-panel').appendChild(node);
+        tree.appendChild(node);
       }
     }
   });
@@ -28,24 +35,24 @@
     
     let nodeElement = document.createElement('div');
     nodeElement.className = "node-view fold";
+    nodeElement.setAttribute('value', nodeName);
     nodeElement.appendChild(nameTag);
     nodeElement.appendChild(addButton);
     nodeElement.appendChild(delButton);
     
     nodeElement.addEventListener('click', (e) => {
       e.stopPropagation();
+      
       if(~e.target.className.indexOf("node-view_node-name")) {
         let thisNode = e.target.parentNode;
-        toggleFold(thisNode);
+        thisNode.classList.toggle("fold");
       } else if(e.target.className === "node-view_button add") {
         let childNodeName = prompt("Child Node Name");
         if(childNodeName) {
           let childNode = createNode(childNodeName);
           let thisNode = e.target.parentNode;
           thisNode.appendChild(childNode);
-          if(~thisNode.className.indexOf("fold")) {
-            thisNode.className = thisNode.className.replace("fold", "");
-          }
+          thisNode.classList.remove('fold');
         }
       } else if(e.target.className === "node-view_button del") {
         let thisNode = e.target.parentNode;
@@ -56,12 +63,24 @@
     return nodeElement;
   }
   
-  function toggleFold(node) {
-    if(~node.className.indexOf("fold")) {
-      node.className = node.className.replace("fold", "");
-    } else {
-      node.className += "fold";
-    }
+  function DFS(rootNode, searchString) {
+    (function recurse(currentNode) {
+      let containTargetNode = false;
+      for(let i = 0, len = currentNode.children.length; i < len; i++) {
+        let childNode = currentNode.children[i];
+        if(childNode.classList.contains('node-view')) {
+          if(recurse(childNode))
+          containTargetNode = recurse(childNode) || containTargetNode;
+        }
+      }
+      
+      let isTargetNode = (currentNode != rootNode && ~currentNode.getAttribute('value').indexOf(searchString));
+      
+      containTargetNode && currentNode.classList.remove('fold');
+      isTargetNode && currentNode.classList.add('highlight');
+      
+      return containTargetNode || isTargetNode;
+    })(rootNode);
   }
   
 })();

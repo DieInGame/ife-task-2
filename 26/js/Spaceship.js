fe1ad15e435c308ce96c91eae01ce9bff104b2a0
A2ZH <thejianmail@gmail.com>
2016-03-29 19:54:31 +0800

    spaceship add update and speed

--- 26/js/Spaceship.js
+++ 26/js/Spaceship.js
@@ -1,11 +1,35 @@
 "use script";
 
 class SpaceShip {
-  constructor(id) {
+  constructor(id, speed, radius) {
     this.__id = id;
     this.__power = 100;
     this.__state = "STOPING";
     this.__active = true;
+    this.__radius = radius;
+    this.__angularVelocity = speed / this.__radius; // Convert linear velocity to angular velocity
+    this.__rotationAngular = 0;
+    this.__consumption = 3;
+    this.__powerGrowth = 1;
+  }
+  
+  update() {
+    if(this.__state === "MOVING") {
+      this.consumePower(this.__consumption);
+      this.__rotationAngle = (this.__rotationAngle + this.__angularVelocity) % Math.PI;
+    }
+    this.charge(this.__powerGrowth);
+    if(this.__power <= 0) {
+      this.stop();
+    }
+  }
+  
+  get active() {
+    return this.__active;
+  }
+  
+  get rotationAngle() {
+    return this.__rotationAngle;
   }
   
   move() {

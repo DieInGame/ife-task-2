ab3feb6dd1a77a6e9ca0e33989411624709493de
A2ZH <thejianmail@gmail.com>
2016-04-01 13:36:00 +0800

    prefix with zero

--- 33/js/javascript.js
+++ 33/js/javascript.js
@@ -110,7 +110,7 @@
   var rowCells = document.querySelectorAll("tr:first-child td");
   var colCells = document.querySelectorAll("tr td:first-child");
   for(let i = 1, len = rowCells.length; i < len; i++) {
-    colCells[i].innerHTML = rowCells[i].innerHTML = (i-1).toString(16);
+    colCells[i].innerHTML = rowCells[i].innerHTML = "0" + (i-1).toString(16);
   }
   
   function submitCommand(rawString) {

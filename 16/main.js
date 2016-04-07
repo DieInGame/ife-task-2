37c6fd969664ca02c312e5040e3a6d3f14b17faf
Fiooona <845559655@qq.com>
2016-04-07 08:40:14 +0800

    Update main.js
    最后的删除按钮绑定有和上面的重复

--- 16/main.js
+++ 16/main.js
@@ -17,13 +17,13 @@ var btn_delete = document.getElementsByClassName("btn-del");
  * 然后渲染aqi-list列表，增加新增的数据
  */
 function addAqiData() {
-    var data_city  = document.getElementById("aqi-city-input").value;
-    var data_value = document.getElementById("aqi-value-input").value;
+    var data_city  = document.getElementById("aqi-city-input").value.trim();
+    var data_value = document.getElementById("aqi-value-input").value.trim();
     if (!data_city.match(/^[\u4e00-\u9fa5aa-zA-z]+$/i)){
         document.getElementById("tip_city").innerHTML="城市只能含中英文字符";
         return ;
     }
-    if(!data_value.match(/^\d*$/i))
+    if(!data_value.match(/^\d/i))
     {
         document.getElementById("tip_value").innerHTML="空气质量只能含数字!";
         return ;
@@ -112,11 +112,11 @@ function init() {
   // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
   btn_add.addEventListener("click",addBtnHandle);
   // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
-    if(btn_delete){
-        for(var x in btn_delete){  
-            btn_delete[x].addEventListener("click",delBtnHandle);
-        }
-    }
+   // if(btn_delete){
+    //    for(var x in btn_delete){  
+     //       btn_delete[x].addEventListener("click",delBtnHandle);
+     //   }
+    //}
   
 }
 

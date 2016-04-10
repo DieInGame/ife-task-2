cb19d0cac6dcdbdbab3ecbe167131084c343b3e3
A2ZH <thejianmail@gmail.com>
2016-04-10 21:30:38 +0800

    add line numbers

--- 35/js/editor.js
+++ 35/js/editor.js
@@ -2,10 +2,12 @@
 
 var Editor = function(editorInfo) {
   this.textarea = null;
+  this.lineNumbers = null;
   this.minWidth = 0;
   this.minHeight = 0;
   this.fontSize = 14;
   this.lineHeight = 20;
+  this.lineCount = 0;
 
   if(!editorInfo) {
     throw new Error('No EditorInfo Provided');
@@ -21,6 +23,10 @@ var Editor = function(editorInfo) {
     throw new Error('no textarea provided');
   }
 
+  if(!this.lineNumbers) {
+    throw new Error('no lineNumbers provided');
+  }
+
   this.textarea.addEventListener('input', () => {
     this.textarea.style.height = this.minHeight; // reset the height of editor
     var height = this.textarea.scrollHeight;
@@ -28,6 +34,17 @@ var Editor = function(editorInfo) {
       height = this.minHeight;
     }
     this.textarea.style.height = height + 'px';
-    console.log(height);
+    this.lines = this.textarea.value.split(/\r*\n/);
+    while(this.lineCount != this.lines.length) {
+      if(this.lineCount < this.lines.length) {
+        this.lineCount += 1;
+        var numbersNode = document.createElement('span');
+        numbersNode.innerHTML = this.lineCount;
+        this.lineNumbers.appendChild(numbersNode);
+      } else {
+        this.lineCount -= 1;
+        this.lineNumbers.removeChild(this.lineNumbers.lastElementChild);
+      }
+    }
   });
 };
--- 35/js/main.js
+++ 35/js/main.js
@@ -1,5 +1,6 @@
 var editor = new Editor({
   textarea: document.getElementsByTagName('textarea')[0],
+  lineNumbers: document.getElementsByClassName('line-nums')[0],
   minWidth: 550,
   minHeight: 620
 });
